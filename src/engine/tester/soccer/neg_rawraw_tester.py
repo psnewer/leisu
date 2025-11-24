bce80f943f40642ea405ca66d8baa603a8c4e4ad
miller <psnewer@163.com>
2025-11-24 19:30:45 +0800

    none

--- src/engine/tester/soccer/neg_rawraw_tester.py
+++ src/engine/tester/soccer/neg_rawraw_tester.py
@@ -38,7 +38,7 @@ class NEG_RAWRAW_TESTER(ABSTRACT_TESTER):
 		return man
 
 	def extract_odds_values(self,row):
-		return np.nan_to_num([float(row['draw_0']),float(row['home']),float(row['draw']),float(row['away'])], nan=0).tolist()
+		return np.nan_to_num([float(row['draw_0']),float(row['home']),float(row['draw']),float(row['away'])], nan=1).tolist()
 
 	def filter_selected(self, row, df_density):
 		filtered_selected = []
--- src/engine/tester/soccer/vs_rawraw_tester.py
+++ src/engine/tester/soccer/vs_rawraw_tester.py
@@ -34,7 +34,7 @@ class VS_RAWRAW_TESTER(ABSTRACT_TESTER):
 
 	def extract_odds_values(self,selected_list):
 		selected_list = json.loads(selected_list)
-		return [np.nan_to_num([item['raw_win'],item['raw_draw']], nan=0).tolist() for item in selected_list]
+		return [np.nan_to_num([item['raw_win'],item['raw_draw']], nan=1).tolist() for item in selected_list]
 
 	def filter_selected(self, row, df_density):
 		filtered_selected = []
